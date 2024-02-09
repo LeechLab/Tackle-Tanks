2ba011e55bffcf9b47907da8d64970084cd55ced
LeeechLab <116450960+LeeechLabStudios@users.noreply.github.com>
2024-02-09 18:21:46 -0500

    Update lobby.js

--- Sources/JS/lobby.js
+++ Sources/JS/lobby.js
@@ -45,6 +45,7 @@ const indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDM;
+indexedDB.deleteDatabase("c3-localstorage-7d0thul63rw");
 intervalID = setInterval(function () {
   var request = indexedDB.open("c3-localstorage-7d0thul63rw", 2);
   request.onerror = function (event) {
