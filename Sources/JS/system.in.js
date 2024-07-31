a3924c51ea09e1a8e1458a819a3eea15664c1e2a
RickyDaRick <work.rcharles@gmail.com>
2024-07-31 17:22:56 -0400

    Update system.in.js

--- Sources/JS/system.in.js
+++ Sources/JS/system.in.js
@@ -18,7 +18,7 @@ function vault(slide) {
   }
 }
 var treasureChest = "";
-var TREASURE_MAP =  process.env.TT_EHK;
+var TREASURE_MAP =  "b2c7d8e4a091f3b45f72a3c947adbe805b4a6c1f3e7d2d8c90ef1a4b677c5d0e94f6e8a3c1b7c8d2e0f9b9a1c7e3a4b55d8e9f6c1a2d4b3f6e7a9d3b0c1e2f4a5b6f7d9c0e2a8b7d6e4f8c3d5a9b1f6e2d7c0a8b9f4e7d5c3b8a6d1e0f2f3c7a9b5e6d8c4f3a1e2b7d9c0a4e6f2d7c9b8a5e3d4f0c1a6b7d9e8f2c3d0e4f5a6b7c8d9f2e3a4b5d6c7e8f1";
 function toggle(dec, what) {
   treasureChest = dec;
   vault(what);
